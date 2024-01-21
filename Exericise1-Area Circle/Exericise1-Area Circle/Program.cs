d391c3a02bbd64fb38e1c9d6a123002a45ead750
Marcela Gonçalves dos Santos <marcela.santos1@uqac.ca>
2024-01-21 12:34:53 -0500

    update examples l1,l2 and l3

---
+++ Exericise1-Area Circle/Exericise1-Area Circle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Formats.Asn1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double area;
+        Console.WriteLine("Enter the radius for the circle: ");
+        double radius = double.Parse(Console.ReadLine());
+        area= Math.PI * Math.Pow(radius,2);
+        Console.WriteLine(area);
+
+    }
+}
---
+++ PredecessorSucessor/PredecessorSucessor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        int number,sucessor,predecessor;
+        Console.WriteLine("Type a number : ");
+        number = int.Parse(Console.ReadLine());
+        sucessor = number + 1;
+        predecessor = number - 1;
+        Console.WriteLine("Sucessor: " + sucessor);
+        Console.WriteLine("Predecessor: "+ predecessor);
+    }
+}
