8c0593b1da18648a01bc6795efb29e7c80a13b33
Marcela Gonçalves dos Santos <marcela.santos1@uqac.ca>
2024-01-21 21:13:11 -0500

    lecture 4 - if...else and switch

---
+++ CheckingNumber/CheckingNumber/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double result = 0, n1, n2;
+        char op;
+        double result2;
+
+        String message;
+
+        Console.WriteLine("Type the first number: ");
+        n1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Type the first number: ");
+        n2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Choose the operation: + - * /");
+        op = char.Parse(Console.ReadLine());
+
+        switch (op)
+        {
+
+            case '+':
+                result = n1 + n2;
+                break;
+
+            case '-':
+                result = n1 - n2;
+                break;
+
+            case '*':
+                result = n1 * n2;
+                break;
+
+            case '/':
+                result = n1 / n2;
+                break;
+        }
+
+        
+
+        result2 = Math.Floor(result);
+        Console.WriteLine("Number 1: " + n1);
+        Console.WriteLine("Number 2: " + n2);
+        Console.WriteLine("Result: " + result);
+
+        Console.WriteLine("Result after Math.Floor operation: " + result2);
+
+        if (result == result2)
+        {
+            message = "The result is integer";
+            if (result % 2 == 0)
+            {
+                message = message+", even"; 
+            }
+            else
+            {
+                message = message + ", odd";
+            }
+        }
+        else
+        {
+            message = "The result is decimal";
+
+        }
+
+        if (result > 0)
+        {
+            message = message + ", positive";
+        }
+        else
+        {
+            message = message + ", negative";
+
+        }
+
+        Console.WriteLine(message);
+
+    }
+}
---
+++ GradeConcept/GradeConcept/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double g1, g2, average;
+        string concept="",message="";
+
+        Console.WriteLine("Type the first grade: ");
+        g1= double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Type the second grade: ");
+        g2 = double.Parse(Console.ReadLine());
+
+        average = (g1 + g2) / 2;
+
+        if (average >= 9)
+        {
+            concept = "A";
+        }
+        else
+        {
+            if (average >=7.5 && average<9) {
+            concept = "B";
+            }
+            else
+            {
+                if(average >=6 && average < 7.5)
+                {
+                    concept= "C";
+                }
+                else
+                {
+                    if(average>=4 && average < 6)
+                    {
+                        concept= "D";
+                    }
+                    else
+                    {
+                        if(average>0 && average < 4)
+                        {
+                            concept= "E";
+                        }
+                        else
+                        {
+                            concept = "I";
+                        }
+                    }
+                }
+            }
+        }
+        if (concept == "A" ||concept== "B" || concept == "C"){
+            message = "PASSED";
+        }
+
+        if (concept == "D" || concept == "E") { 
+            message = "FAIL";
+        }
+
+        Console.WriteLine("Final result = " + message);
+
+
+    }
+}
